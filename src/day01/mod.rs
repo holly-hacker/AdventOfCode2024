e53d2b9684fb3c3a208bcf6570685f37bcbea546
HoLLy <dario.db@hotmail.com>
2024-12-01 06:54:47 +0100

    Add optimized solution for day 1
    Not much to optimize, mostly integer parsing and changing O(n^2) lookups with a pseudo-hashmap.
    Using an actual hashmap seemed to be slower, especially for the sample cases, so I used a sorted vec and binary search for lookups.
    
    Benchmarks indicated that part 1 (real input) went from 51us to 47us and part 2 from 75us to 50us, but the results were very noisy.

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,5 @@
+use utils::fast_parse_int;
+
 use super::*;
 
 pub struct Day;
@@ -8,14 +10,14 @@ impl SolutionSilver<usize> for Day {
     const INPUT_REAL: &'static str = include_str!("input_real.txt");
 
     fn calculate_silver(input: &str) -> usize {
-        let mut nums1 = vec![];
-        let mut nums2 = vec![];
+        let mut nums1 = Vec::with_capacity(1000);
+        let mut nums2 = Vec::with_capacity(1000);
         input
             .lines()
             .map(|line| line.split_once("   ").unwrap())
             .for_each(|(left, right)| {
-                nums1.push(left.parse::<usize>().unwrap());
-                nums2.push(right.parse::<usize>().unwrap());
+                nums1.push(fast_parse_int(left));
+                nums2.push(fast_parse_int(right));
             });
 
         nums1.sort();
@@ -33,6 +35,9 @@ impl SolutionGold<usize, usize> for Day {
     const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
 
     fn calculate_gold(input: &str) -> usize {
+        return Self::calculate_gold_opt(input);
+
+        #[allow(unreachable_code)]
         let mut nums1 = vec![];
         let mut nums2 = vec![];
         input
@@ -46,6 +51,49 @@ impl SolutionGold<usize, usize> for Day {
         nums1
             .into_iter()
             .map(|n| nums2.iter().filter(|m| **m == n).count() * n)
+            .sum()
+    }
+}
+
+impl Day {
+    fn calculate_gold_opt(input: &str) -> usize {
+        let mut nums1 = Vec::with_capacity(1000);
+        let mut nums2 = Vec::with_capacity(1000);
+        input
+            .lines()
+            .map(|line| line.split_once("   ").unwrap())
+            .for_each(|(left, right)| {
+                nums1.push(fast_parse_int(left));
+                // *nums2.entry(fast_parse_int(right)).or_default() += 1;
+                nums2.push(fast_parse_int(right));
+            });
+
+        // convert nums2 into a pseudo-hashmap
+        nums2.sort();
+        let mut nums2_counts = vec![];
+        let mut current_num = nums2[0];
+        let mut current_count = 0;
+        for num in nums2.into_iter() {
+            if num == current_num {
+                current_count += 1;
+            } else {
+                nums2_counts.push((current_num, current_count));
+                current_num = num;
+                current_count = 1;
+            }
+        }
+        if current_count > 0 {
+            nums2_counts.push((current_num, current_count));
+        }
+
+        nums1
+            .into_iter()
+            .map(|num| {
+                num * nums2_counts
+                    .binary_search_by(|&(k, _)| k.cmp(&num))
+                    .map(|pos| nums2_counts[pos].1)
+                    .unwrap_or_default()
+            })
             .sum()
     }
 }
