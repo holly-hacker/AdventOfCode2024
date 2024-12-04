4bf35dbc967ad89c8d9778275fe290163c444f8b
HoLLy <dario.db@hotmail.com>
2024-12-04 07:00:45 +0100

    Add optimized solution for day 4
    No significant improvements in part 2, but a bunch of smaller improvements make part 1 almost 3x as fast.
    
    Optimizations for part 1:
    - Instead of creating a vec for a grid, index into the input buffer directly. This removes the initial setup cost and removes 1 bounds check per lookup.
    - Check for horizontal matches using a standard search algorithm which can utilize SIMD. These bytes are next to eachother in memory which makes this possible.
    - When finding an the start or end of the needle, check in both directions to eliminate redundant lookups in the buffer and thus bounds checks.
    - Early exit if the start or end of the needle wasn't found. No need to check in each direction then.
    
    The first of these optimizations is also applied to part 2, but does not seem to result in a major performance improvement.
    
    These optimizations also improve the performance of the sample inputs by a lot, mostly due to the removed startup cost of creating the grid.
    
    Before optimizations:
    Day 04 silver (sample)  time:   [1.4932 µs 1.4944 µs 1.4957 µs]
    Day 04 silver (real)    time:   [413.36 µs 413.73 µs 414.30 µs]
    Day 04 gold (sample)    time:   [340.51 ns 340.77 ns 341.03 ns]
    Day 04 gold (real)      time:   [22.890 µs 22.918 µs 22.959 µs]
    
    After optimizations:
    Day 04 silver (sample)  time:   [403.67 ns 404.03 ns 404.60 ns]
    Day 04 silver (real)    time:   [141.97 µs 142.17 µs 142.41 µs]
    Day 04 gold (sample)    time:   [63.146 ns 63.283 ns 63.449 ns]
    Day 04 gold (real)      time:   [20.018 µs 20.038 µs 20.064 µs]

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -8,26 +8,26 @@ impl SolutionSilver<usize> for Day {
     const INPUT_REAL: &'static str = include_str!("input_real.txt");
 
     fn calculate_silver(input: &str) -> usize {
-        let h = input.lines().count();
-        let w = input.lines().next().unwrap().trim_end().len();
-        let grid: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
+        let input = input.as_bytes();
+        let w = input.iter().position(|&c| c == b'\n').unwrap();
+        let stride = w + 1;
+        let h = (input.len() + 1) / stride;
 
+        // left and right are fastest to check since those bytes are consecutive
+        // we can use standard search algorithms for that which utilize SIMD
+        let count_ltr = memchr::memmem::find_iter(input, b"XMAS").count();
+        let count_rtl = memchr::memmem::find_iter(input, b"SAMX").count();
+
+        // read the other directions normally
         let mut count = 0;
         for y in 0..h {
             for x in 0..w {
-                for (dir_x, dir_y) in [
-                    (-1, -1),
-                    (-1, 0),
-                    (-1, 1),
-                    (0, -1),
-                    (0, 1),
-                    (1, -1),
-                    (1, 0),
-                    (1, 1),
-                ] {
-                    if dir_x < 0 && x < 3 {
-                        continue;
-                    }
+                let cur_pos = input[y * stride + x];
+                if cur_pos != b'X' && cur_pos != b'S' {
+                    continue;
+                }
+
+                for (dir_x, dir_y) in [(0, 1), (1, -1), (1, 1)] {
                     if dir_x > 0 && x + 3 >= w {
                         continue;
                     }
@@ -46,18 +46,22 @@ impl SolutionSilver<usize> for Day {
                     let x2 = (x as isize + dir_x as isize * 2) as usize;
                     let x3 = (x as isize + dir_x as isize * 3) as usize;
 
-                    if grid[y][x] == b'X'
-                        && grid[y1][x1] == b'M'
-                        && grid[y2][x2] == b'A'
-                        && grid[y3][x3] == b'S'
-                    {
+                    let read = [
+                        cur_pos,
+                        input[y1 * stride + x1],
+                        input[y2 * stride + x2],
+                        input[y3 * stride + x3],
+                    ];
+
+                    // check for a match in both directions
+                    if &read == b"XMAS" || &read == b"SAMX" {
                         count += 1;
                     }
                 }
             }
         }
 
-        count
+        count_ltr + count_rtl + count
     }
 }
 
@@ -65,25 +69,29 @@ impl SolutionGold<usize, usize> for Day {
     const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
 
     fn calculate_gold(input: &str) -> usize {
-        let h = input.lines().count();
-        let w = input.lines().next().unwrap().trim_end().len();
-        let grid: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
+        let input = input.as_bytes();
+        let w = input.iter().position(|&c| c == b'\n').unwrap();
+        let stride = w + 1;
+        let h = (input.len() + 1) / stride;
 
         let mut count = 0;
         for y in 1..h - 1 {
             for x in 1..w - 1 {
                 // check center of the cross
-                if grid[y][x] != b'A' {
+                if input[y * stride + x] != b'A' {
                     continue;
                 }
 
-                // check diagonal top-left to bottom-right
-                let check1 = (grid[y - 1][x - 1] == b'M' && grid[y + 1][x + 1] == b'S')
-                    || (grid[y + 1][x + 1] == b'M' && grid[y - 1][x - 1] == b'S');
+                // check diagonals
+                let top_left = input[(y - 1) * stride + x - 1];
+                let top_right = input[(y - 1) * stride + x + 1];
+                let bottom_left = input[(y + 1) * stride + x - 1];
+                let bottom_right = input[(y + 1) * stride + x + 1];
 
-                // check diagonal top-right to bottom-left
-                let check2 = (grid[y + 1][x - 1] == b'M' && grid[y - 1][x + 1] == b'S')
-                    || (grid[y - 1][x + 1] == b'M' && grid[y + 1][x - 1] == b'S');
+                let check1 = (top_left == b'M' && bottom_right == b'S')
+                    || (bottom_right == b'M' && top_left == b'S');
+                let check2 = (bottom_left == b'M' && top_right == b'S')
+                    || (top_right == b'M' && bottom_left == b'S');
 
                 if check1 && check2 {
                     count += 1
