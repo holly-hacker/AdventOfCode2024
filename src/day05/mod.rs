a04f9a53a9eb1fb226e14fec6d23cf9f4329bdf4
HoLLy <dario.db@hotmail.com>
2024-12-05 10:40:43 +0100

    Add initial optimization pass for day 5 part 1
    All of this mostly comes down to reducing heap allocations and dynamically sized operations. It mostly comes from assuming that all numbers will be 2 digits long.
    
    For rules, we can assume each line is 6 characters long (2 digits, a pipe, 2 digits, a newline) meaning we no longer have to seek to find any constant characters. We can get the length of the rules segment and then index into the slice. Parsing is as easy as getting the first and second digit and adding them together.
    Rules are stored in an array of ArrayVecs. This is entirely stored on the heap and of fixed size. Each item in the array contains a list of the second part of the rule, where the index is the first part of the rule. 11 is subtracted from the indices because 11 is the first possible number.
    
    Updates are no longer stored in a temporary vec (this was already redundant) and are now parsed in a similar way, by indexing into each line.
    After parsing, they are checked against the rules by iterating over each number in the line and keeping a stack buffer of the numbers seen so far. This means we don't have to iterate over all the previous numbers in the line each time.
    
    I've only optimized part 1 because part 2 is much of the same, and I have more stuff planned before I get to that.
    
    Day 05 silver (sample)  time:   [196.46 ns 197.08 ns 197.83 ns]
                            change: [-84.896% -84.862% -84.827%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    Day 05 silver (real)    time:   [26.928 µs 26.975 µs 27.032 µs]
                            change: [-97.474% -97.467% -97.460%] (p = 0.00 < 0.05)
                            Performance has improved.

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,3 +1,4 @@
+use tinyvec::ArrayVec;
 use utils::fast_parse_int;
 
 use super::*;
@@ -10,33 +11,45 @@ impl SolutionSilver<usize> for Day {
     const INPUT_REAL: &'static str = include_str!("input_real.txt");
 
     fn calculate_silver(input: &str) -> usize {
-        let (rules, updates) = input.split_once("\n\n").unwrap();
-        let rules: Vec<(usize, usize)> = rules
-            .lines()
-            .map(|line| {
-                let (l, r) = line.split_once('|').unwrap();
-                (fast_parse_int(l), fast_parse_int(r))
-            })
-            .collect();
-        let updates: Vec<Vec<usize>> = updates
-            .lines()
-            .map(|line| line.split(',').map(fast_parse_int).collect())
-            .collect();
+        let input = input.as_bytes();
+        let split_index = memchr::memmem::find(input, b"\n\n").unwrap();
+        let rules_slice = &input[..split_index];
+        let updates_slice = &input[(split_index + 2)..];
 
-        updates
-            .into_iter()
-            .filter(|u| {
-                rules.iter().all(|r| {
-                    match (
-                        u.iter().position(|&x| x == r.0),
-                        u.iter().position(|&x| x == r.1),
-                    ) {
-                        (Some(p0), Some(p1)) => p0 <= p1,
-                        _ => true,
-                    }
+        // all numbers seem to be from 11 to 99 (inclusive)
+        let mut rules = [ArrayVec::<[u8; 24]>::new(); 99 - 11 + 1];
+
+        let rule_count = rules_slice.len() / 6;
+        (0..rule_count).for_each(|rule_idx| {
+            let left =
+                (rules_slice[rule_idx * 6] - b'0') * 10 + rules_slice[rule_idx * 6 + 1] - b'0';
+            let right =
+                (rules_slice[rule_idx * 6 + 3] - b'0') * 10 + rules_slice[rule_idx * 6 + 4] - b'0';
+            rules[left as usize - 11].push(right);
+        });
+
+        updates_slice
+            .split(|&b| b == b'\n')
+            .map(|line| {
+                let num_count = (line.len() + 1) / 3;
+                (0..num_count)
+                    .map(|num_i| (line[num_i * 3] - b'0') * 10 + line[num_i * 3 + 1] - b'0')
+                    .collect::<ArrayVec<[u8; 23]>>()
+            })
+            .filter(|update| {
+                let mut seen = [false; 99 - 11 + 1];
+                (0..update.len()).all(|idx| {
+                    let cur = update[idx];
+                    let rules = &rules[(cur - 11) as usize];
+
+                    let ok = rules.is_empty() || rules.iter().all(|&r| !seen[r as usize - 11]);
+
+                    seen[cur as usize - 11] = true;
+
+                    ok
                 })
             })
-            .map(|u| u[u.len() / 2])
+            .map(|u| u[u.len() / 2] as usize)
             .sum()
     }
 }
