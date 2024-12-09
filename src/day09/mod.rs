d1054e39cf82e0bc38c5c83d1feb99c88e701b6a
HoLLy <dario.db@hotmail.com>
2024-12-09 07:47:20 +0100

    Optimize day 9
    Basically use ranges for part 1 as well. This makes it a lot more complex though.
    For part 2, don't expand the disk at the end.
    
    Day 09 silver (sample)  time:   [208.64 ns 208.94 ns 209.27 ns]
    Day 09 silver (real)    time:   [55.675 ms 55.982 ms 56.290 ms]
    Day 09 gold (sample)    time:   [183.59 ns 183.96 ns 184.32 ns]
    Day 09 gold (real)      time:   [79.757 ms 79.879 ms 79.995 ms]

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -10,51 +10,7 @@ impl SolutionSilver<usize> for Day {
     fn calculate_silver(input: &str) -> usize {
         let input = input.as_bytes();
 
-        let mut disk = vec![];
-        let mut is_file = true;
-        let mut last_file_id = 0usize;
-        for file in input {
-            let file_len = (file - b'0') as usize;
-            if is_file {
-                for _ in 0..file_len {
-                    disk.push(Some(last_file_id));
-                }
-                last_file_id += 1;
-            } else {
-                for _ in 0..file_len {
-                    disk.push(None);
-                }
-            }
-
-            is_file = !is_file;
-        }
-
-        // compact
-        'outer: for i in (1..disk.len()).rev() {
-            if let Some(file_id) = disk[i] {
-                for j in 0..i {
-                    if disk[j].is_none() {
-                        disk[j] = Some(file_id);
-                        disk[i] = None;
-                        continue 'outer;
-                    }
-                }
-            }
-        }
-
-        disk.into_iter()
-            .enumerate()
-            .fold(0, |acc, (i, x)| acc + i * x.unwrap_or_default())
-    }
-}
-
-impl SolutionGold<usize, usize> for Day {
-    const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
-
-    fn calculate_gold(input: &str) -> usize {
-        let input = input.as_bytes();
-
-        let mut disk = vec![];
+        let mut disk = Vec::with_capacity(input.len());
         let mut is_file = true;
         let mut last_file_id = 0usize;
         for file in input {
@@ -70,14 +26,111 @@ impl SolutionGold<usize, usize> for Day {
         }
 
         // compact
-        for i in (1..disk.len()).rev() {
+        let mut i_src = disk.len() - 1;
+        'outer: loop {
+            if disk[i_src].0.is_none() {
+                i_src -= 1;
+                continue;
+            }
+
+            let mut i_dst = 0;
+            loop {
+                // skip file blocks and empty blocks
+                if disk[i_dst].0.is_some() || disk[i_dst].1 == 0 {
+                    i_dst += 1;
+                    continue;
+                }
+
+                // move what we can
+                let to_move = disk[i_dst].1.min(disk[i_src].1);
+                let left_over = disk[i_src].1 - to_move;
+                let val = disk[i_src].0;
+                if to_move == 0 {
+                    break 'outer; // weird edge condition for the end of the compacted disk
+                }
+                debug_assert_ne!(to_move, 0);
+                debug_assert!(val.is_some());
+
+                disk[i_src].1 -= to_move;
+                disk[i_dst].1 -= to_move;
+                debug_assert_eq!(disk[i_src].1, left_over);
+
+                // insert before dest
+                disk.insert(i_dst, (val, to_move)); // TODO: slow bc moves everything after it
+                i_src += 1;
+
+                if disk[i_src].1 == 0 {
+                    disk[i_src].0 = None;
+                    i_src -= 1;
+                    break;
+                }
+
+                i_dst += 1;
+                if i_dst >= i_src {
+                    break;
+                }
+            }
+
+            i_src -= 1;
+            if i_src == 0 {
+                break;
+            }
+        }
+
+        disk.into_iter()
+            .fold((0, 0), |(acc_idx, acc_checksum), (file_val, file_len)| {
+                if let Some(file_val) = file_val {
+                    (
+                        acc_idx + file_len,
+                        acc_checksum
+                            + match file_len {
+                                0 => 0,
+                                _ => {
+                                    let range_center = acc_idx as f32 + (file_len - 1) as f32 * 0.5;
+                                    let range_indices_sum =
+                                        (range_center * file_len as f32) as usize;
+                                    range_indices_sum * file_val
+                                }
+                            },
+                    )
+                } else {
+                    (acc_idx + file_len, acc_checksum)
+                }
+            })
+            .1
+    }
+}
+
+impl SolutionGold<usize, usize> for Day {
+    const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
+
+    fn calculate_gold(input: &str) -> usize {
+        let input = input.as_bytes();
+
+        let mut disk = Vec::with_capacity(input.len());
+        let mut is_file = true;
+        let mut last_file_id = 0usize;
+        for file in input {
+            let file_len = (file - b'0') as usize;
+            if is_file {
+                disk.push((Some(last_file_id), file_len));
+                last_file_id += 1;
+            } else {
+                disk.push((None, file_len));
+            }
+
+            is_file = !is_file;
+        }
+
+        // compact
+        for i in (0..disk.len()).rev() {
             if let (Some(file_id), file_len) = disk[i] {
                 for j in 0..i {
                     if let (None, empty_len) = disk[j] {
                         if empty_len >= file_len {
                             disk[i] = (None, file_len);
 
-                            disk.insert(j, (Some(file_id), file_len));
+                            disk.insert(j, (Some(file_id), file_len)); // TODO: slow bc moves everything after it
                             disk[j + 1].1 -= file_len;
                             break;
                         }
@@ -86,24 +139,27 @@ impl SolutionGold<usize, usize> for Day {
             }
         }
 
-        // expand
-        let mut expanded_disk = vec![];
-        for (file_id, file_len) in disk {
-            if let Some(file_id) = file_id {
-                for _ in 0..file_len {
-                    expanded_disk.push(Some(file_id));
+        disk.into_iter()
+            .fold((0, 0), |(acc_idx, acc_checksum), (file_val, file_len)| {
+                if let Some(file_val) = file_val {
+                    (
+                        acc_idx + file_len,
+                        acc_checksum
+                            + match file_len {
+                                0 => 0,
+                                _ => {
+                                    let range_center = acc_idx as f32 + (file_len - 1) as f32 * 0.5;
+                                    let range_indices_sum =
+                                        (range_center * file_len as f32) as usize;
+                                    range_indices_sum * file_val
+                                }
+                            },
+                    )
+                } else {
+                    (acc_idx + file_len, acc_checksum)
                 }
-            } else {
-                for _ in 0..file_len {
-                    expanded_disk.push(None);
-                }
-            }
-        }
-
-        expanded_disk
-            .into_iter()
-            .enumerate()
-            .fold(0, |acc, (i, x)| acc + i * x.unwrap_or_default())
+            })
+            .1
     }
 }
 
